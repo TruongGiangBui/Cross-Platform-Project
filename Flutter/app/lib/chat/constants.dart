e123e223809f23ee170ff288fb28b5dc8b15481c
winner31082000 <winner310800@gmail.com>
2021-11-13 23:20:31 +0700

    Change kPrimaryColor

--- Flutter/app/lib/chat/constants.dart
+++ Flutter/app/lib/chat/constants.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-const kPrimaryColor = Color(0xff9511a8);
+const kPrimaryColor = Color(0xff5D46B7);
 const kSecondaryColor = Color(0xFFFE9901);
 const kContentColorLightTheme = Color(0xFF1D1D35);
 const kContentColorDarkTheme = Color(0xFFF5FCF9);
